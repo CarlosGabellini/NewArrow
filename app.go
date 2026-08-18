3df9e7269763d5b7011cf50773601fc6c41e101c
Gabellini <CarlosGabellini@proton.me>
2026-08-18 17:01:17 -0300

    Primeiro modelo da Nav, adicionando o icone de pasta e definindo o CSS

--- app.go
+++ app.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	play "NewArrow/internalGo/player"
 	"context"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
-	play.Gerador1
 	
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{
-		Gerador1: play.Gerador1{},
-	}
+	return &App{}
 }
