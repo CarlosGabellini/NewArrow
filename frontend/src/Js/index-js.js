ed2868249257788107c0cbff4be492e5771c4518
Gabellini <CarlosGabellini@proton.me>
2026-08-19 17:14:39 -0300

    Adicionando comentarios do codigo sobre diretorios
    Localizacao e como devem funcionar a partir do momento que tiver um binario.

--- frontend/src/Js/index-js.js
+++ frontend/src/Js/index-js.js
@@ -1,5 +1,9 @@
 import { ToqueMusica } from '../../wailsjs/go/main/App.js';
 
+//Os imports devem ter EXATAMENTE o nome da funcao exata (Eh case sensitive); e tambem colocar
+//o caminho exato, na hora de gerar o binario tambem vou ter que dar uma olhada sobre como isso
+//funciona para distribuicao;
+
 const tocarMusica = document.getElementById("btn-player");
 
 async function tocar() {
@@ -11,5 +15,5 @@ async function tocar() {
     }
 }
 
-
+//Este eh um exemplo de como a funcao no JS deve funcionar, sempre acompanhada do Async e await;
 tocarMusica.addEventListener("click", tocar);
--- interno/player/tocador.go
+++ interno/player/tocador.go
@@ -18,12 +18,16 @@ func (t *Tocador) ToqueMusica() error {
 	//Para tocar a musica, o caminho nao pode ser relativo, no Wails, o caminho sempre comeca
 	// a partir da raiz do projeto, e entao selecionamos a pasta a partir disso, nao posso me
 	// esquecer de fazer isso.
-	f, err := os.Open("interno/diretorioMusicas/ABBA - Dancing Queen (Official Music Video).mp3")
+	f, err := os.Open("interno/diretorioMusicas/FRONT LINE ASSEMBLY (Feat. Jimmy Urine) - Rock Me Amadeus.mp3")
+
+	//Outra pegadinha, na hora de fazer o binario, nao pode colocar estes caminhos, o binario altera como funciona
+	//o caminho das pastas adicionadas, isso tambem acontece na hora de localizar as outras paginas no html.
 
 	if err != nil {
 		return err
 	}
 
+	//O que faz a magica acontecer aqui;
 	stream, format, err := mp3.Decode(f)
 
 	if err != nil {
