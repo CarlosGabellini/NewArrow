3a1af38de41a787047a30c387e3bbf37e9a5df1e
Gabellini <CarlosGabellini@proton.me>
2026-08-20 14:26:54 -0300

    Adicionando as funcoes para tocar o audio corretamente
    declarando a struct em conjunto com a biblioteca beep para fazer rodar
    o audio de forma correta. Essa eh a primeira parte de como deve ficar.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module NewArrow
 go 1.25.0
 
 require (
+	github.com/gopxl/beep v1.4.1
 	github.com/gopxl/beep/v2 v2.1.1
 	github.com/wailsapp/wails/v2 v2.14.0
 )
--- interno/player/tocador.go
+++ interno/player/tocador.go
@@ -2,44 +2,35 @@ package player
 
 import (
 	"os"
+	"time"
+
 	"github.com/gopxl/beep/v2"
 	"github.com/gopxl/beep/v2/mp3"
 	"github.com/gopxl/beep/v2/speaker"
-	"time"
 )
 
-type Tocador struct {
-	ctrl *beep.Ctrl
-	stream beep.StreamSeekCloser
+type PlayingMusic struct {
+	Erro error
+	stremando beep.StreamSeekCloser
+	formato beep.Format
+	Controle *beep.Ctrl
 }
 
-func (t *Tocador) ToqueMusica() error {
-
-	//Para tocar a musica, o caminho nao pode ser relativo, no Wails, o caminho sempre comeca
-	// a partir da raiz do projeto, e entao selecionamos a pasta a partir disso, nao posso me
-	// esquecer de fazer isso.
-	f, err := os.Open("interno/diretorioMusicas/FRONT LINE ASSEMBLY (Feat. Jimmy Urine) - Rock Me Amadeus.mp3")
-
-	//Outra pegadinha, na hora de fazer o binario, nao pode colocar estes caminhos, o binario altera como funciona
-	//o caminho das pastas adicionadas, isso tambem acontece na hora de localizar as outras paginas no html.
+func CarregarMusica(caminho string) (P PlayingMusic) {
+	f, err := os.Open(caminho)
 
 	if err != nil {
-		return err
+		P.Erro = err
+		return P
 	}
 
-	//O que faz a magica acontecer aqui;
-	stream, format, err := mp3.Decode(f)
+	P.stremando, P.formato, err = mp3.Decode(f)
+	P.Erro = err
 
-	if err != nil {
-		return err
-	}
+	return P
+}
 
-	t.stream = stream
-
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-	t.ctrl = &beep.Ctrl{Streamer: stream, Paused: false}
-	speaker.Play(t.ctrl)
-
+func (P PlayingMusic) IniciarSpeaker() error {
+	speaker.Init(P.formato.SampleRate, P.formato.SampleRate.N(time.Second / 20))
 	return nil
 }
