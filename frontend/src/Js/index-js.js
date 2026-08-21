f2de9ac7ec961c2077066aca5d97985dff88e94f
Gabellini <CarlosGabellini@proton.me>
2026-08-21 15:29:54 -0300

    Adicionando funcao de pause no JS

--- frontend/src/Js/index-js.js
+++ frontend/src/Js/index-js.js
@@ -1,19 +1,43 @@
 import { PlayTrack } from '../../wailsjs/go/main/App.js';
+import { TogglePause } from '../../wailsjs/go/main/App.js';
 
 //Os imports devem ter EXATAMENTE o nome da funcao exata (Eh case sensitive); e tambem colocar
 //o caminho exato, na hora de gerar o binario tambem vou ter que dar uma olhada sobre como isso
 //funciona para distribuicao;
 
 const tocarMusica = document.getElementById("btn-player");
+let estaTocando = false;
+let informationConsole = true;
 
-async function tocar() {
-    try {
-      await PlayTrack();
-      console.log('Tocando!');
-    } catch (err) {
-    console.log("erro ao tocar: ", err);
+async function tocarMusic() {
+  
+  try {  
+    if (estaTocando) {
+      await TogglePause();
+
+      if (informationConsole) {
+        console.log('Pausado!');
+        informationConsole = false;
+      } else {
+        console.log("despausado!");
+        informationConsole = true;
+      }
+      
+      estaTocando = true;
     }
+
+    else {
+      await PlayTrack();
+      console.log("musica tocando agora!");
+      estaTocando = true;
+    }
+  }
+ 
+  catch (error) {
+    console.log("erro: " + error);
+  }
 }
 
+tocarMusica.addEventListener("click", tocarMusic);
+
 //Este eh um exemplo de como a funcao no JS deve funcionar, sempre acompanhada do Async e await;
-tocarMusica.addEventListener("click", tocar);
--- interno/player/toqueMusica.go
+++ interno/player/toqueMusica.go
@@ -4,7 +4,7 @@ var PlayerMusic PlayingMusic
 
 func ToqueMusica() error {
 
-	PlayerMusic = CarregarMusica("interno/diretorioMusicas/ABBA - Dancing Queen (Official Music Video).mp3")
+	PlayerMusic = CarregarMusica("interno/diretorioMusicas/Tu Pai.mp3")
 
 	if PlayerMusic.Erro != nil {
 		return PlayerMusic.Erro
