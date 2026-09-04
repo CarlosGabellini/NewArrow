a105bb5f3585f00e88d4deb6a35dbc5557c0bffc
Gabellini <CarlosGabellini@proton.me>
2026-09-04 16:59:23 -0300

    Fazendo a barra de progresso simples para as musicas

--- frontend/src/Js/index-js.js
+++ frontend/src/Js/index-js.js
@@ -14,6 +14,8 @@ const _lista_de_musicas = await RetornandoLista();
 
 //Os query Selector ficam nessa parte aqui!
 const music_painel = document.querySelector(".music-painel");
+const barra_progresso = document.querySelector(".barra-progresso");
+const barra_progresso_interna = document.querySelector(".barra-progresso-preenchida");
 
 //A lista do for implementa a lista de musica que o go fez em um loop e adiciona ao html.
 
@@ -45,9 +47,25 @@ for (let i = 0; i < _lista_de_musicas.length; i++) {
 music_painel.addEventListener("click", (event) => {
   let elemento = event.target.closest(".faixa-de-musica");
   if (!elemento) return;
-  
   let numero_da_musica = parseInt(elemento.dataset.index);
 
   _meu_player_music.load(`/music/${encodeURIComponent(_lista_de_musicas[numero_da_musica].caminho_path)}`);
-  _meu_player_music.play()
+  _meu_player_music.play();
 });
+
+barra_progresso.addEventListener("click", (event) => {
+  
+  let retangulo = barra_progresso.getBoundingClientRect();
+  
+  let clique_x = event.clientX - retangulo.left; // posição do clique relativa à barra
+  let porcentagem_clicada = clique_x / retangulo.width;
+  
+  let novo_tempo = porcentagem_clicada * _meu_player_music.audio.duration;
+    _meu_player_music.seek(novo_tempo);
+})
+
+
+_meu_player_music.onProgress = (currentTime, duration) => {
+  let porcentagem = currentTime / duration * 100;
+  barra_progresso_interna.style.width = `${porcentagem}%`;
+}
